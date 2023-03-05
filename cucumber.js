aae33c0dd001f1ca6fefcfd662e34d4a7d0f6a52
ILLIA SKAPCHUK <ilya.skapchuk@gmail.com>
2023-03-05 19:12:50 +0200

    Added Cucumber HTML reporting

--- cucumber.js
+++ cucumber.js
@@ -1,9 +1,8 @@
-//require: ['./step_definitions/*.steps.ts']
-
 const options = [
   "--require-module ts-node/register",
   "--require ./step_definitions/*.steps.ts",
   "--format progress",
+  "--format json:cucumber_report.json"
 ].join(" ");
 
 const runFeatures = ["./features/", options].join(" ");
